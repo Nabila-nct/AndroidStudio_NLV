9718db2eec599528f04429f650250488909e09fc
Nabila-nct <202260290@ucc.mx>
2025-09-17 23:55:33 -0600

    animacion en android

--- MainActivity.java
+++ MainActivity.java
@@ -1,22 +1,22 @@
-package com.example.demofragmentos;
+package com.example.animationwithframes;
 
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
-import android.os.Handler;
-import android.view.View;
-import android.widget.Button;
+import android.view.MotionEvent;
+import android.view.animation.Animation;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-import androidx.fragment.app.FragmentContainerView;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
 
 public class MainActivity extends AppCompatActivity {
-  Button btn;
-  FragmentContainerView fragmentoContenedor;
+
+  private AnimationDrawable animationKid;
+
+  private ImageView imagen;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,30 +24,21 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
-        btn=findViewById(R.id.button);
-
-        Fragmento fr=new Fragmento();
-
-        FragmentoDos fdos=new FragmentoDos();
-        FragmentManager fm= getSupportFragmentManager();
-        FragmentTransaction ft=fm.beginTransaction();
-
-        btn.setOnClickListener(new View.OnClickListener() {
-          @Override
-          public void onClick(View v) {
-            ft.replace(R.id.aqui_va_fragmento, fdos);
-            ft.commit();
-            Handler handler=new Handler();
-            handler.postDelayed(new Runnable() {
-              @Override
-              public void run() {
-                ft.replace(R.id.aqui_va_fragmento,fr);
-                ft.addToBackStack(null);
-                ft.commit();
-              }
-            },5000);
-          }
-        });
-
+        imagen=findViewById(R.id.imageAnimation);
+        imagen.setBackgroundResource(R.drawable.kid_animation);
+        animationKid=(AnimationDrawable) imagen.getBackground();
+        animationKid.run();
     }
+
+  @Override
+  public boolean onTouchEvent(MotionEvent event) {
+      if(event.getAction()==MotionEvent.ACTION_DOWN){
+        if(animationKid.isRunning()){
+          animationKid.stop();
+        }else{
+          animationKid.start();
+        }
+      }
+    return super.onTouchEvent(event);
+  }
 }
