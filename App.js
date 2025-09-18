8a273db92bfe9082fc24308eef8b058778afe063
Nabila-nct <202260290@ucc.mx>
2025-09-18 00:06:54 -0600

    Primera App React

--- App.js
+++ App.js
@@ -1,62 +1,28 @@
-import { useEffect, useState } from 'react';
-import { ActivityIndicator, FlatList, StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import { Map, Modal, Panel } from './componentes';
 
 export default function App() {
-  const [users, setUsers]= useState([])
-  const[loading, setLoading]=useState(true)
-
-  useEffect(()=>{
-    fetch('https://jsonplaceholder.typicode.com/todos')
-    .then(response=>response.json())
-    .then(data=> {
-      setUsers(data)
-      setLoading(false)
-    })
-  },[])
-
-if(loading){
-  return(<View style={styles.center}> <Text >Cargando...</Text> 
- <ActivityIndicator size ='large' color= '#00f'/>
-  </View> )
-}
-
   return (
     <View style={styles.container}>
-      <Text>Consumiendo una API</Text>
-      <FlatList  
-        data={users}
-        renderItem={({item})=><View style={styles.item}>
-          <Text>{item.title}</Text>
-          <Text>{item.completed ? "✅" : "❌"}</Text>
-        </View>}
-        keyExtractor={item=>String(item.id)}
-      />
-
+      <Map />
+      <Panel />
+      <Modal />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  center: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+
+  },
+
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
-  center:{
-    flex:1,
-    backgroundColor: '#ffffffff',
-    alignItems:'center',
-    justifyContent:'center',
-
-  }, 
-  item:{
-    flexDirection:'row',
-    justifyContent:'space-between',
-    backgroundColor:"#a2d2ff", 
-    padding:20,
-    marginVertical:8, 
-    marginHorizontal:16,  
-    borderRadius:10,
-  }
 });
